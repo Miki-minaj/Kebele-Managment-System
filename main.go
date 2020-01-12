aa5d243c6a05145343e0436b5df3f1ef55957c25
miki-minaj <haylemikaeltefera@gmail.com>
2020-01-12 23:28:15 +0300

    the changed hndler on main

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"net/http"
 	"text/template"
 
+	"github.com/julienschmidt/httprouter"
 	_ "github.com/lib/pq"
 	"github.com/miki-minaj/Kebele-Managment-System/handler"
+	api "github.com/miki-minaj/Kebele-Managment-System/handler/api"
 	mrepim "github.com/miki-minaj/Kebele-Managment-System/menu/repository"
 	msrvim "github.com/miki-minaj/Kebele-Managment-System/menu/service"
 )
@@ -22,33 +24,6 @@ const (
 
 var templ = template.Must(template.ParseGlob("templates/*.html"))
 
-func index(w http.ResponseWriter, r *http.Request) {
-	// var err error
-
-	// rows, err := db.Query("SELECT id,name FROM infos")
-	// if err != nil {
-	// 	//fmt.Println("error")
-	// }
-	// xc := []info{}
-	// for rows.Next() {
-	// 	in := info{}
-	// 	//var id int
-	// 	//var name string
-
-	// 	rows.Scan(&in.ID, &in.Name)
-
-	// 	xc = append(xc, in)
-
-	// 	//panic(err)
-	// 	//fmt.Println("id | name ")
-	// 	//fmt.Println(in.ID, in.Name)
-	// }
-
-	//w.Write([]byte("<h1>Hello World!</h1>"))
-	//info := personalinfo{"Miki",20}
-	templ.ExecuteTemplate(w, "kebele.html", "hey")
-
-}
 func main() {
 	var db *sql.DB
 	var err error
@@ -61,15 +36,23 @@ func main() {
 	}
 
 	catagoryRepo := mrepim.NewCategoryRepositoryImpl(db)
-
 	categoryServ := msrvim.NewCategoryService(catagoryRepo)
 	adminhandler := handler.NewAdminCategoryHandler(templ, categoryServ)
+	navhandler := handler.NewMenuHandler(templ, categoryServ)
 
+	apiinforepo := mrepim.NewCategoryRepositoryImpl(db)
+	apiinfoserv := msrvim.NewCategoryService(apiinforepo)
+	apiadmininfohandler := api.NewAdminCategoryHandler(apiinfoserv)
+
+	router := httprouter.New()
+
+	router.POST("/v1/admin/infos", apiadmininfohandler.AdminCategoriesNew)
+	router.GET("/v1/admin/infos", apiadmininfohandler.AdminCategories)
 	//http.HandleFunc("/f", insert)
 	fs := http.FileServer(http.Dir("./templates/asset"))
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", navhandler.Index)
 	http.HandleFunc("/f", adminhandler.AdminCategoriesNew)
 	http.HandleFunc("/s", adminhandler.AdminCategories)
 	http.Handle("/asset/", http.StripPrefix("/asset/", fs))
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", router)
 }
