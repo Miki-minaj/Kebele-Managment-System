fc9e6cdc50d7568be3adbf14ffe70d0a8cb2f29f
Henock-Yonas <henockyeyoni@gmail.com>
2020-01-12 22:52:40 +0300

    interface implementation

--- menu/repository.go
+++ menu/repository.go
@@ -3,7 +3,9 @@ package menu
 import "github.com/miki-minaj/Kebele-Managment-System/entity"
 
 // CategoryRepository specifies food menu category database operations
-type CategoryRepository interface {
+type InfoRepository interface {
 	StoreCategory(category *entity.Category) (*entity.Category, error)
 	Categories() ([]entity.Category, error)
+	DeleteCategory(id int) error
+	Category(id int) (entity.Category, error)
 }
--- menu/service.go
+++ menu/service.go
@@ -3,7 +3,9 @@ package menu
 import "github.com/miki-minaj/Kebele-Managment-System/entity"
 
 // CategoryService specifies food menu category services
-type CategoryService interface {
+type InfoService interface {
 	StoreCategory(category *entity.Category) (*entity.Category, error)
 	Categories() ([]entity.Category, error)
+	DeleteCategory(id int) error
+	Category(id int) (entity.Category, error)
 }
